a9b462d9f5f6952e81c5f1c02405659ea124bca3
Pablo Gaza <p.garza1983@gmail.com>
2024-01-03 23:45:09 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,75 @@
+import "./src/styles/main.css";
+import VanillaTilt from 'vanilla-tilt';
+// import { Collapse } from "flowbite";
+
+let cursorX = 0;
+let cursorY = 0;
+const cursorSize = 130; // Set the size of your cursor
+
+document.addEventListener("mousemove", (e) => {
+  cursorX = e.clientX - cursorSize / 2;
+  cursorY = e.clientY - cursorSize / 2;
+  requestAnimationFrame(updateCursor);
+});
+
+document.addEventListener("scroll", function () {
+  requestAnimationFrame(updateCursor);
+});
+
+function updateCursor() {
+  const cursor = document.querySelector(".cursor");
+  cursor.style.width = `${cursorSize}px`;
+  cursor.style.height = `${cursorSize}px`;
+  cursor.style.transform = `translate(${cursorX}px, ${
+    cursorY + window.scrollY
+  }px)`;
+}
+
+const heroParallax = document.getElementById("hero-parallax");
+const heroTitle = document.getElementById("hero_title");
+const projects = document.getElementById("projects");
+
+document.addEventListener("scroll", function () {
+  const scrollPosition = window.scrollY;
+  heroParallax.style.transform = "translateY(" + scrollPosition * 1.5 + "px)";
+  heroParallax.style.opacity = 1 - scrollPosition / 600;
+  heroTitle.style.transform = "translateY(" + scrollPosition * 0.5 + "px)";
+  heroTitle.style.opacity = 2.5 - scrollPosition / 500;
+  // projects.style.opacity = 3 - scrollPosition / 600;
+});
+
+const observerRight = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show-right");
+      } else {
+        entry.target.classList.remove("show-right");
+      }
+    });
+  }
+  // { threshold: 0.5 }
+);
+
+const hiddenElementsRight = document.querySelectorAll(".observer-right");
+hiddenElementsRight.forEach((element) => {
+  observerRight.observe(element);
+});
+
+const observerLeft = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show-left");
+      } else {
+        entry.target.classList.remove("show-left");
+      }
+    });
+  }
+  // { threshold: 0.5 }
+);
+
+const hiddenElementsLeft = document.querySelectorAll(".observer-left");
+hiddenElementsLeft.forEach((element) => {
+  observerLeft.observe(element);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.html",
+    "./src/**/*.{html,js,mjs}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {
+      dropShadow: {
+        "2xl": "0 0px 35px rgba(255, 255, 255, 0.35)",
+      },
+      colors: {
+        primary: "",
+        secondary: "#3f1eb4",
+        tertiary: "#ff00ff",
+        
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
+
