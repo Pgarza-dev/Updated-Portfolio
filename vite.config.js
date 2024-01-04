02e90d8d0a32f2a10b820e3ceab83ee8af95b8e3
Pablo Gaza <p.garza1983@gmail.com>
2024-01-04 09:52:40 +0100

    Update file paths in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, "src/index.html"),
+        main: path.resolve(__dirname, "index.html"),
         about: path.resolve(__dirname, "src/about/about.html"),
         projects: path.resolve(__dirname, "src/projects/projects.html"),
       },
