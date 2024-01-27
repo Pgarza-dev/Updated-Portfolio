d7dc4084fb849a8ccf3ba959958cb11378474261
Pablo Gaza <p.garza1983@gmail.com>
2024-01-27 14:20:45 +0100

    Update navigation , screen size responsiveness

---
+++ src/hamburger-menu/hamburger.js
@@ -0,0 +1,16 @@
+// set the target element that will be collapsed or expanded (eg. navbar menu)
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
