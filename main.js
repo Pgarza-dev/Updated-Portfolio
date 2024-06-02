e27a28033df98a54c8f24482d4a33fd5d86ab6ad
Pablo Gaza <p.garza1983@gmail.com>
2024-06-02 21:53:46 +0200

    Update main.js with reduced tilt max value and remove unused code

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import {hamburgerMenu} from './src/utils/hamburger.js'
 import VanillaTilt from 'vanilla-tilt'
 
 VanillaTilt.init(document.querySelectorAll('.card'), {
- max: 25,
+ max: 10,
  speed: 400,
  glare: true,
  'max-glare': 0.5,
@@ -13,7 +13,7 @@ hamburgerMenu()
 
 let cursorX = 0
 let cursorY = 0
-const cursorSize = 130 // Set the size of your cursor
+const cursorSize = 130 
 
 document.addEventListener('mousemove', (e) => {
  cursorX = e.clientX - cursorSize / 2
@@ -36,7 +36,7 @@ function updateCursor() {
 
 const heroParallax = document.getElementById('hero-parallax')
 const heroTitle = document.getElementById('hero_title')
-const projects = document.getElementById('projects')
+
 
 document.addEventListener('scroll', function () {
  const scrollPosition = window.scrollY
@@ -44,7 +44,6 @@ document.addEventListener('scroll', function () {
  heroParallax.style.opacity = 1 - scrollPosition / 600
  heroTitle.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)'
  heroTitle.style.opacity = 2.5 - scrollPosition / 500
- // projects.style.opacity = 3 - scrollPosition / 600;
 })
 
 const observerRight = new IntersectionObserver(
@@ -83,17 +82,17 @@ hiddenElementsLeft.forEach((element) => {
  observerLeft.observe(element)
 })
 
-function detectBrowserAndApplyStyles() {
- const body = document.body
- const isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1
+// function detectBrowserAndApplyStyles() {
+//  const body = document.body
+//  const isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1
 
- if (isFirefox) {
-  // If user is using Firefox, remove .static-tv and add .bg-image
-  body.classList.remove('static-tv')
-  body.classList.add('bg-image')
- }
-}
+//  if (isFirefox) {
+//   // If user is using Firefox, remove .static-tv and add .bg-image
+//   body.classList.remove('static-tv')
+//   body.classList.add('bg-image')
+//  }
+// }
 
-window.onload = function () {
- detectBrowserAndApplyStyles()
-}
+// window.onload = function () {
+//  detectBrowserAndApplyStyles()
+// }
