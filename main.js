7d9dd8340d56c1f52bf1191f9bf076c1dafa338f
Pablo Gaza <p.garza1983@gmail.com>
2024-06-02 15:02:11 +0200

    Update navigation menu links and styling

--- main.js
+++ main.js
@@ -1,100 +1,100 @@
-import "./src/styles/main.css";
-import { hamburgerMenu } from "./src/utils/hamburger.js";
-import VanillaTilt from "vanilla-tilt";
+import './src/styles/main.css'
+import {hamburgerMenu} from './src/utils/hamburger.js'
+import VanillaTilt from 'vanilla-tilt'
 // import { Collapse } from "flowbite";
 
-VanillaTilt.init(document.querySelectorAll(".card"), {
-  max: 25,
-  speed: 400,
-  glare: true,
-  "max-glare": 0.5,
-});
+VanillaTilt.init(document.querySelectorAll('.card'), {
+ max: 25,
+ speed: 400,
+ glare: true,
+ 'max-glare': 0.5,
+})
 
-hamburgerMenu();
+hamburgerMenu()
 
-let cursorX = 0;
-let cursorY = 0;
-const cursorSize = 130; // Set the size of your cursor
+let cursorX = 0
+let cursorY = 0
+const cursorSize = 130 // Set the size of your cursor
 
-document.addEventListener("mousemove", (e) => {
-  cursorX = e.clientX - cursorSize / 2;
-  cursorY = e.clientY - cursorSize / 2;
-  requestAnimationFrame(updateCursor);
-});
+document.addEventListener('mousemove', (e) => {
+ cursorX = e.clientX - cursorSize / 2
+ cursorY = e.clientY - cursorSize / 2
+ requestAnimationFrame(updateCursor)
+})
 
-document.addEventListener("scroll", function () {
-  requestAnimationFrame(updateCursor);
-});
+document.addEventListener('scroll', function () {
+ requestAnimationFrame(updateCursor)
+})
 
 function updateCursor() {
-  const cursor = document.querySelector(".cursor");
-  cursor.style.width = `${cursorSize}px`;
-  cursor.style.height = `${cursorSize}px`;
-  cursor.style.transform = `translate(${cursorX}px, ${
-    cursorY + window.scrollY
-  }px)`;
+ const cursor = document.querySelector('.cursor')
+ cursor.style.width = `${cursorSize}px`
+ cursor.style.height = `${cursorSize}px`
+ cursor.style.transform = `translate(${cursorX}px, ${
+  cursorY + window.scrollY
+ }px)`
 }
 
-const heroParallax = document.getElementById("hero-parallax");
-const heroTitle = document.getElementById("hero_title");
-const projects = document.getElementById("projects");
+const heroParallax = document.getElementById('hero-parallax')
+const heroTitle = document.getElementById('hero_title')
+const projects = document.getElementById('projects')
 
-document.addEventListener("scroll", function () {
-  const scrollPosition = window.scrollY;
-  heroParallax.style.transform = "translateY(" + scrollPosition * 1.5 + "px)";
-  heroParallax.style.opacity = 1 - scrollPosition / 600;
-  heroTitle.style.transform = "translateY(" + scrollPosition * 0.5 + "px)";
-  heroTitle.style.opacity = 2.5 - scrollPosition / 500;
-  // projects.style.opacity = 3 - scrollPosition / 600;
-});
+document.addEventListener('scroll', function () {
+ const scrollPosition = window.scrollY
+ heroParallax.style.transform = 'translateY(' + scrollPosition * 1.5 + 'px)'
+ heroParallax.style.opacity = 1 - scrollPosition / 600
+ heroTitle.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)'
+ heroTitle.style.opacity = 2.5 - scrollPosition / 500
+ // projects.style.opacity = 3 - scrollPosition / 600;
+})
 
 const observerRight = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show-right");
-      } else {
-        entry.target.classList.remove("show-right");
-      }
-    });
-  },
-  // { threshold: 0.5 }
-);
+ (entries) => {
+  entries.forEach((entry) => {
+   if (entry.isIntersecting) {
+    entry.target.classList.add('show-right')
+   } else {
+    entry.target.classList.remove('show-right')
+   }
+  })
+ },
+ { threshold: 0.5 }
+)
 
-const hiddenElementsRight = document.querySelectorAll(".observer-right");
+const hiddenElementsRight = document.querySelectorAll('.observer-right')
 hiddenElementsRight.forEach((element) => {
-  observerRight.observe(element);
-});
+ observerRight.observe(element)
+})
 
 const observerLeft = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show-left");
-      } else {
-        entry.target.classList.remove("show-left");
-      }
-    });
-  },
-  // { threshold: 0.5 }
-);
+ (entries) => {
+  entries.forEach((entry) => {
+   if (entry.isIntersecting) {
+    entry.target.classList.add('show-left')
+   } else {
+    entry.target.classList.remove('show-left')
+   }
+  })
+ },
+ { threshold: 0.5 }
+)
 
-const hiddenElementsLeft = document.querySelectorAll(".observer-left");
+const hiddenElementsLeft = document.querySelectorAll('.observer-left')
 hiddenElementsLeft.forEach((element) => {
-  observerLeft.observe(element);
-});
+ observerLeft.observe(element)
+})
 
 function detectBrowserAndApplyStyles() {
-  const body = document.body;
-  const isFirefox = navigator.userAgent.toLowerCase().indexOf("firefox") > -1;
+ const body = document.body
+ const isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1
 
-  if (isFirefox) {
-    // If user is using Firefox, remove .static-tv and add .bg-image
-    body.classList.remove("static-tv");
-    body.classList.add("bg-image");
-  }
+ if (isFirefox) {
+  // If user is using Firefox, remove .static-tv and add .bg-image
+  body.classList.remove('static-tv')
+  body.classList.add('bg-image')
+ }
 }
 
 window.onload = function () {
-  detectBrowserAndApplyStyles();
-};
+ detectBrowserAndApplyStyles()
+}
--- src/utils/animation.js
+++ src/utils/animation.js
@@ -1,89 +1,73 @@
-const homeLink = document.getElementById("home-link");
-const projectLink = document.getElementById("project-link");
-const skillsLink = document.getElementById("skills-link");
-const aboutLink = document.getElementById("about-link");
-const hamburgerMenu = document.getElementById("hamburger-menu");
-const navMenu = document.getElementById("navMenu");
+const homeLink = document.getElementById('home-link')
+const projectLink = document.getElementById('project-link')
+const aboutLink = document.getElementById('about-link')
+const hamburgerMenu = document.getElementById('hamburger-menu')
+const navMenu = document.getElementById('navMenu')
 
 export function homeButton() {
-  homeLink.addEventListener("click", () => {
-    hamburgerMenu.classList.toggle("active");
-    navMenu.classList.toggle("active");
+ homeLink.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('active')
+  navMenu.classList.toggle('active')
 
-    const isMenuActive = hamburgerMenu.classList.contains("active");
+  const isMenuActive = hamburgerMenu.classList.contains('active')
 
-    if (!isMenuActive) {
-      console.log("Hamburger menu is closed");
-    }
-  });
+  if (!isMenuActive) {
+   console.log('Hamburger menu is closed')
+  }
+ })
 }
 
 export function projectButton() {
-  projectLink.addEventListener("click", () => {
-    hamburgerMenu.classList.toggle("active");
-    navMenu.classList.toggle("active");
+ projectLink.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('active')
+  navMenu.classList.toggle('active')
 
-    const isMenuActive = hamburgerMenu.classList.contains("active");
+  const isMenuActive = hamburgerMenu.classList.contains('active')
 
-    if (!isMenuActive) {
-      console.log("Hamburger menu is closed");
-    }
-  });
-}
-
-export function skillsButton() {
-  skillsLink.addEventListener("click", () => {
-    hamburgerMenu.classList.toggle("active");
-    navMenu.classList.toggle("active");
-
-    const isMenuActive = hamburgerMenu.classList.contains("active");
-
-    if (!isMenuActive) {
-      console.log("Hamburger menu is closed");
-    }
-  });
+  if (!isMenuActive) {
+   console.log('Hamburger menu is closed')
+  }
+ })
 }
 
 export function aboutButton() {
-  aboutLink.addEventListener("click", () => {
-    hamburgerMenu.classList.toggle("active");
-    navMenu.classList.toggle("active");
+ aboutLink.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('active')
+  navMenu.classList.toggle('active')
 
-    const isMenuActive = hamburgerMenu.classList.contains("active");
+  const isMenuActive = hamburgerMenu.classList.contains('active')
 
-    if (!isMenuActive) {
-      console.log("Hamburger menu is closed");
-    }
-  });
+  if (!isMenuActive) {
+   console.log('Hamburger menu is closed')
+  }
+ })
 }
 
 const enhanceText = (id) => {
-  const element = document.getElementById(id);
+ const element = document.getElementById(id)
 
-  if (element) {
-    const text = element.textContent.split("");
-    element.textContent = "";
+ if (element) {
+  const text = element.textContent.split('')
+  element.textContent = ''
 
-    text.forEach((letter) => {
-      const span = document.createElement("span");
-      span.className = "letter";
-      span.textContent = letter;
-      element.append(span);
-    });
-  }
-};
+  text.forEach((letter) => {
+   const span = document.createElement('span')
+   span.className = 'letter'
+   span.textContent = letter
+   element.append(span)
+  })
+ }
+}
 
-homeButton();
-skillsButton();
-aboutButton();
-enhanceText("project-link");
-enhanceText("skills-link");
-enhanceText("about-link");
-enhanceText("home-link");
-enhanceText("about-me");
-enhanceText("gmail-link");
-enhanceText("linkedIn-link");
-enhanceText("gitHub-link");
-enhanceText("gmail-footer-link");
-enhanceText("linkedIn-footer-link");
-enhanceText("gitHub-footer-link");
+homeButton()
+aboutButton()
+enhanceText('project-link')
+enhanceText('about-link')
+enhanceText('home-link')
+enhanceText('about-me')
+enhanceText('gmail-link')
+enhanceText('linkedIn-link')
+enhanceText('gitHub-link')
+enhanceText('gmail-footer-link')
+enhanceText('linkedIn-footer-link')
+enhanceText('gitHub-footer-link')
